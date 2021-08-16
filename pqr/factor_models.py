b23606d5470841ffb4e20f064f80f6be6e5dd462
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-08-16 21:56:58 +0300

    adapt interfaces

--- pqr/factor_models.py
+++ pqr/factor_models.py
@@ -1,30 +1,22 @@
 """
-This module contains all necessary functionality to create factor models.
-Factor model is the set of portfolios, covering all stock universe in each
-period. It is used to test whether any factor is present on the market or not,
-that is why full coverage of stock universe is important.
+This module contains all necessary functionality to create factor models. Factor model is the set of
+portfolios, covering all stock universe in each period. It is used to test whether any factor is
+present on the market or not, that is why full coverage of stock universe is important.
 
-Usually factor models are built for single-factors with the quantiles method.
-So, only this way to fit factor models is natively supported, but if you need
-to make full coverage of stock universe with time-series method or top-n
-portfolios, you can make it by yourself, using portfolios module.
+Usually factor models are built for single-factors with the quantiles method. So, only this way to
+fit factor models is natively supported, but if you need to make full coverage of stock universe
+with time-series method or top-n portfolios, you can make it by yourself, using portfolios module.
 
-Users of factor models are often interested in performance of the
-wml-portfolio, and this functionality is also supported.
+Users of factor models are often interested in performance of the wml-portfolio, and this
+functionality is also supported.
 
-Moreover, you can calibrate factor model parameters, using simple bruteforce
-method, provided by grid_search() function.
+Moreover, you can calibrate factor model parameters, using simple bruteforce method, provided by
+grid_search() function.
 """
-
-from __future__ import annotations
-
-from typing import Tuple, List, Optional, Iterable, Dict, Literal
 
 import numpy as np
 import pandas as pd
 
-import pqr.benchmarks
-import pqr.factors
 import pqr.portfolios
 
 __all__ = [
@@ -35,16 +27,8 @@ __all__ = [
 ]
 
 
-def fit_factor_model(
-        stock_prices: pd.DataFrame,
-        factor: pqr.factors.Factor,
-        weighting_factor: Optional[pqr.factors.Factor] = None,
-        balance: Optional[int | float] = None,
-        fee_rate: int | float = 0,
-        quantiles: int = 3,
-        add_wml: bool = False,
-        is_bigger_better: bool = True
-) -> List[pqr.portfolios.AbstractPortfolio]:
+def fit_factor_model(stock_prices, factor, weighting_factor=None, balance=None, fee_rate=0,
+                     quantiles=3, add_wml=False, is_bigger_better=True):
     """
     Fits factor model with quantile-method.
 
@@ -53,23 +37,28 @@ def fit_factor_model(
 
     Parameters
     ----------
-    stock_prices
+    stock_prices : pd.DataFrame
         Prices, representing stock universe.
-    factor
+    factor : Factor
         Factor to pick stocks into the portfolio.
-    weighting_factor
+    weighting_factor : Factor, optional
         Factor to weigh picks.
-    balance
+    balance : int or float, optional
         Initial balance of the portfolio.
-    fee_rate
+    fee_rate : int or float, default=0
         Indicative commission rate for every deal.
-    quantiles
+    quantiles : int > 1, default=3
         Number of portfolios to build for covering stock universe by quantiles.
-    add_wml
+    add_wml : bool, default=False
         Whether to also create wml-portfolio or not.
-    is_bigger_better
+    is_bigger_better : bool, default=True
         Whether portfolio with highest quantiles will be "winners" for
         wml-portfolio or with lowest ones.
+
+    Returns
+    -------
+    list of AbstractPortfolio
+        Factor portfolios, covering all stock universe (optionally, with wml-portfolio).
     """
 
     quantiles_ = _split_quantiles(quantiles)
@@ -78,54 +67,47 @@ def fit_factor_model(
     for q in quantiles_:
         portfolio = pqr.portfolios.Portfolio('q({:.2f}, {:.2f})'.format(*q))
         portfolio.pick_stocks_by_factor(factor, q)
-
         if weighting_factor is not None:
             portfolio.weigh_by_factor(factor)
         else:
             portfolio.weigh_equally()
-
         portfolio.allocate(stock_prices, balance, fee_rate)
 
         portfolios.append(portfolio)
 
     if add_wml:
         if is_bigger_better:
-            wml = pqr.portfolios.WmlPortfolio(portfolios[-1],
-                                              portfolios[0])
+            wml = pqr.portfolios.WmlPortfolio(portfolios[-1], portfolios[0])
         else:
-            wml = pqr.portfolios.WmlPortfolio(portfolios[0],
-                                              portfolios[-1])
+            wml = pqr.portfolios.WmlPortfolio(portfolios[0], portfolios[-1])
         portfolios.append(wml)
 
     return portfolios
 
 
-def calculate_portfolios_summary_stats(
-        *portfolios: pqr.portfolios.AbstractPortfolio,
-        benchmark: pqr.benchmarks.Benchmark
-) -> pd.DataFrame:
+def calculate_portfolios_summary_stats(portfolios, benchmark):
     """
     Calculates portfolios summary statistics and gather them into a table.
 
-    See function pqr.metrics.summary().
+    See :func:`~pqr.metrics.summary`.
 
     Parameters
     ----------
-    portfolios
+    portfolios : AbstractPortfolio
         Portfolios, for which summary stats are calculated.
-    benchmark
+    benchmark : AbstractPortfolio or Benchmark
         Benchmark to compute some metrics.
+
+    Returns
+    -------
+    pd.DataFrame
+        Table with metrics for all given `portfolios`.
     """
 
-    stats = pd.DataFrame(
-        [pqr.metrics.summary(p, benchmark) for p in portfolios]).T.round(2)
-    return stats
+    return pd.DataFrame([pqr.metrics.summary(p, benchmark) for p in portfolios]).T.round(2)
 
 
-def factor_model_tear_sheet(
-        *portfolios: pqr.portfolios.AbstractPortfolio,
-        benchmark: pqr.benchmarks.Benchmark,
-) -> pd.DataFrame:
+def factor_model_tear_sheet(portfolios, benchmark):
     """
     Shows the performance assessment of a factor model' portfolios.
 
@@ -136,29 +118,24 @@ def factor_model_tear_sheet(
 
     Parameters
     ----------
-    portfolios
+    portfolios : sequence of AbstractPortfolio
         Portfolios, included into the factor model.
-    benchmark
+    benchmark : AbstractPortfolio or Benchmark
         Benchmark to compute some metrics.
+
+    Returns
+    -------
+    pd.DataFrame
+        Table with summary stats.
     """
 
-    stats = calculate_portfolios_summary_stats(*portfolios,
-                                               benchmark=benchmark)
-    pqr.visualization.plot_cumulative_returns(*portfolios, benchmark=benchmark)
+    stats = calculate_portfolios_summary_stats(portfolios, benchmark)
+    pqr.visualization.plot_cumulative_returns(portfolios, benchmark)
     return stats
 
 
-def grid_search(
-        stock_prices: pd.DataFrame,
-        factor: pd.DataFrame,
-        looking_back_periods: Iterable[int],
-        method: Literal['static', 'dynamic'],
-        lag_periods: Iterable[int],
-        holding_periods: Iterable[int],
-        benchmark: pqr.benchmarks.Benchmark,
-        mask: Optional[pd.DataFrame] = None,
-        **kwargs
-) -> Dict[Tuple[int, int, int], pd.DataFrame]:
+def grid_search(stock_prices, factor_data, looking_back_periods, method, lag_periods,
+                holding_periods, benchmark, mask=None, **kwargs):
     """
     Fits a grid of factor models.
 
@@ -167,50 +144,50 @@ def grid_search(
 
     Parameters
     ----------
-    stock_prices
+    stock_prices : pd.DataFrame
         Prices, representing stock universe.
-    factor
+    factor_data : pd.DataFrame
         Factor, used to pick stocks from (filtered) stock universe.
-    looking_back_periods
+    looking_back_periods : sequence of int > 0
         Looking back periods for `factor` to be tested.
-    method
+    method : {'static', 'dynamic'}
         Whether absolute values of `factor` are used to make decision or
         their percentage changes.
-    lag_periods
+    lag_periods : sequence of int >= 0
         Lag periods for `factor` to be tested.
-    holding_periods
+    holding_periods : sequence of int > 0
         Holding periods for `factor` to be tested.
-    benchmark
+    benchmark : Benchmark
         Benchmark to compare with portfolios and calculate some metrics.
-    mask
+    mask : pd.DataFrame, optional
         Matrix of True/False, where True means that a value should remain
         in `factor` and False - that a value should be deleted.
     **kwargs
-        Keyword arguments for fitting factor models. See fit_factor_model().
+        Keyword arguments for fitting factor models. See
+        :func:`~pqr.factor_models.fit_factor_model`.
+
+    Returns
+    -------
+    dict
+        Dict, where key is the combination of looking back, lag and holding periods and value is a
+        table with summary stats.
     """
 
     results = {}
     for looking in looking_back_periods:
         for lag in lag_periods:
             for holding in holding_periods:
-                transformed_factor = pqr.factors.Factor(factor)
-                transformed_factor.transform(looking, method, lag, holding)
+                factor = pqr.factors.Factor(factor_data)
+                factor.look_back(looking, method).lag(lag).hold(holding)
                 if mask is not None:
-                    transformed_factor.prefilter(mask)
-                portfolios = fit_factor_model(stock_prices, transformed_factor,
-                                              **kwargs)
-                results[(looking, lag, holding)] = (
-                    calculate_portfolios_summary_stats(*portfolios,
-                                                       benchmark=benchmark)
-                )
+                    factor.prefilter(mask)
+                portfolios = fit_factor_model(stock_prices, factor, **kwargs)
+                results[(looking, lag, holding)] = calculate_portfolios_summary_stats(portfolios,
+                                                                                      benchmark)
     return results
 
 
-def _split_quantiles(n: int) -> List[Tuple[int | float, int | float]]:
-    """
-    Creates `n` quantiles, covering all range from 0 to 1.
-    """
-
+def _split_quantiles(n):
     quantile_pairs = np.take(np.linspace(0, 1, n + 1),
                              np.arange(n * 2).reshape((n, -1)) -
                              np.indices((n, 2))[0])
