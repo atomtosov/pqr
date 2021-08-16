499d9f0cc802f09b3dbdcd50dec630605ca55efb
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-08-16 21:27:33 +0300

    remove typing for backwards-compat

--- pqr/benchmarks.py
+++ pqr/benchmarks.py
@@ -1,26 +1,20 @@
 """
-This module provides instruments to create benchmarks. Benchmarks treated as
-theoretical portfolios, which each investor dreams to beat. Usually there are
-already good benchmarks - indices (e.g. S&P 500 or IMOEX). But if for some
-reason you cannot find suitable benchmark you can build it from stock universe
-(with filters and weights, but without selecting stocks). The benchmark will
-include all available (filtered) stock universe in each period  without
-transaction costs, but with weighting positions (optionally).
+This module provides instruments to create benchmarks. Benchmarks treated as theoretical portfolios,
+which each investor dreams to beat. Usually there are already good benchmarks - indices
+(e.g. S&P 500 or IMOEX). But if for some reason you cannot find suitable benchmark you can build it
+from stock universe (with filters and weights, but without selecting stocks). The benchmark will
+include all available (filtered) stock universe in each period  without transaction costs, but with
+weighting positions (optionally).
 
-In most cases you need a benchmark just to compare its performance with
-performance of a portfolio, but do not forget, that portfolios are also can be
-used as benchmarks for calculating metrics/plotting the performance. If you
-want to create benchmark with selecting stocks, just construct the portfolio.
+In most cases you need a benchmark just to compare its performance with performance of a portfolio,
+but do not forget, that portfolios are also can be used as benchmarks for calculating
+metrics/plotting the performance. If you want to create benchmark with selecting stocks, just
+construct the portfolio.
 """
-
-from __future__ import annotations
-
-from typing import Optional
 
 import numpy as np
 import pandas as pd
 
-import pqr.factors
 import pqr.portfolios
 
 __all__ = [
@@ -34,7 +28,7 @@ class Benchmark:
 
     Parameters
     ----------
-    name
+    name : str, default='benchmark'
         Name of the benchmark.
     """
 
@@ -43,26 +37,31 @@ class Benchmark:
     returns: pd.Series
     """Periodical returns of the benchmark (non-cumulative)."""
 
-    def __init__(self, name: str = 'benchmark'):
+    def __init__(self, name='benchmark'):
         self.name = name
 
         self.returns = pd.Series()
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f'Benchmark({repr(self.name)})'
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
-    def from_index(self, index_values: pd.Series) -> Benchmark:
+    def from_index(self, index_values):
         """
         Creates benchmark from existing index (e.g. S&P500 or IMOEX).
 
         Parameters
         ----------
-        index_values
-            Series of index values. Percentage changes of these values are used
-            as returns of the benchmark.
+        index_values : pd.Series
+            Series of index values. Percentage changes of these values are used as returns of the
+            benchmark.
+
+        Returns
+        -------
+        Benchmark
+            Benchmark with filled returns.
         """
 
         self.returns = index_values.pct_change()
@@ -70,36 +69,34 @@ class Benchmark:
 
         return self
 
-    def from_stock_universe(
-            self,
-            stock_prices: pd.DataFrame,
-            mask: Optional[pd.DataFrame] = None,
-            weighting_factor: Optional[pqr.factors.Factor] = None
-    ) -> Benchmark:
+    def from_stock_universe(self, stock_prices, mask=None, weighting_factor=None):
         """
         Creates benchmark from stock universe.
 
-        This type of a benchmark should be used, when there is no relevant
-        index to compare with portfolios (e.g. if stock universe is too
-        specific or just is severe filtered). In other cases it is highly
-        recommended to download data for some existing benchmark, because it is
+        This type of a benchmark should be used, when there is no relevant index to compare with
+        portfolios (e.g. if stock universe is too specific or just is severe filtered). In other
+        cases it is highly recommended to download data for some existing benchmark, because it is
         really hard to accurately replicate any real stock index.
 
-        To build a benchmark from stock universe you can use filtration and
-        weighting, but not picking the stocks. If picking is needed, just
-        construct the portfolio, it also can be used as benchmark.
+        To build a benchmark from stock universe you can use filtration and weighting, but not
+        picking the stocks. If picking is needed, just construct the portfolio, it also can be used
+        as benchmark.
 
         Parameters
         ----------
-        stock_prices
-            Prices, representing stock universe. All of available (not np.nan)
-            for selecting stocks are picked into the "benchmark-portfolio".
-        mask
+        stock_prices : pd.DataFrame
+            Prices, representing stock universe. All of available (not np.nan) for selecting stocks
+            are picked into the "benchmark-portfolio".
+        mask : pd.DataFrame, optional
             Mask to filter stock universe.
-        weighting_factor
-            Factor, weighting positions (if you want to replicate some
-            market-cap weighted benchmark). If not passed, weighs positions
-            equally.
+        weighting_factor : Factor, optional
+            Factor, weighting positions (if you want to replicate some market-cap weighted
+            benchmark). If not passed, weighs positions equally.
+
+        Returns
+        -------
+        Benchmark
+            Benchmark with filled returns.
         """
 
         benchmark_portfolio = pqr.portfolios.Portfolio()
