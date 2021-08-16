b2ca6311827fc1439095c939cc10b8563671e94c
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-08-16 21:24:25 +0300

    adapt zero-int test to new metrics interface

--- pqr/tests.py
+++ pqr/tests.py
@@ -1,9 +1,8 @@
 """
-This module contains instruments to test, whether performance of a portfolio is
-statistically significant or not. The process of statistical testing is an
-important part, when developing a portfolio, because it is very easy to fall
-into the trap: you can get a portfolio with very high total returns, but these
-returns are gotten randomly, so it is not likely that you wanna use this
+This module contains instruments to test, whether performance of a portfolio is statistically
+significant or not. The process of statistical testing is an important part, when developing
+a portfolio, because it is very easy to fall into the trap: you can get a portfolio with very high
+total returns, but these returns are gotten randomly, so it is not likely that you wanna use this
 strategy in future.
 """
 
@@ -19,8 +18,7 @@ __all__ = [
 ]
 
 
-def zero_intelligence_test(stock_prices, portfolio, target_metric, quantiles,
-                           **kwargs):
+def zero_intelligence_test(stock_prices, portfolio, target_metric, quantiles, **kwargs):
     """
     Creates random portfolios, replicating positions of `portfolio`.
 
@@ -31,7 +29,7 @@ def zero_intelligence_test(stock_prices, portfolio, target_metric, quantiles,
     ----------
     stock_prices : pd.DataFrame
         Prices, representing stock universe.
-    portfolio : pqr.Portfolio
+    portfolio : Portfolio
         Portfolio, to be tested (replicated by random portfolios).
     target_metric : callable
         Function-like object, computing some metric (e.g. value of metric). It
@@ -39,40 +37,35 @@ def zero_intelligence_test(stock_prices, portfolio, target_metric, quantiles,
     quantiles : int > 1
         How many quantile-bounds to generate.
     **kwargs
-        Keyword arguments for building up portfolios. See random_portfolios().
+        Keyword arguments for building up portfolios. See
+        :func:`~pqr.portfolios.generate_random_portfolios`.
 
     Returns
     -------
     dict
     """
 
-    random_portfolios = pqr.portfolios.generate_random_portfolios(stock_prices,
-                                                                  portfolio,
-                                                                  **kwargs)
+    random_portfolios = pqr.portfolios.generate_random_portfolios(stock_prices, portfolio, **kwargs)
+    target_values = pd.Series([target_metric(p.returns) for p in random_portfolios])
 
-    target_values = pd.Series([target_metric(p) for p in random_portfolios])
-
-    indices = []
-    for q in np.linspace(0, 1, quantiles):
-        indices.append(
-            target_values[target_values <= target_values.quantile(q)].argmax())
-
+    indices = [target_values[target_values <= target_values.quantile(q)].argmax()
+               for q in np.linspace(0, 1, quantiles)]
     target_quantiles = target_values[indices].sort_values()
+    target_portfolios = np.array(random_portfolios)[target_quantiles.index]
 
-    return (target_quantiles,
-            np.array(random_portfolios)[target_quantiles.index])
+    return {quantile: portfolio for quantile, portfolio in zip(target_quantiles, target_portfolios)}
 
 
 def t_test(portfolio, risk_free_rate=0):
     """
-    Calculates t-statistic and p-value of portfolio returns.
+    Calculates t-statistic and p-value of `portfolio` returns.
 
-    Implements one-sided t-test. Null hypothesis is that `portfolio` returns
-    are greater than `risk_free_rate`.
+    Implements one-sided t-test. Null hypothesis is that `portfolio` returns are greater than
+    `risk_free_rate`.
 
     Parameters
     ----------
-    portfolio : pqr.AbstractPortfolio
+    portfolio : AbstractPortfolio
         An allocated portfolio.
     risk_free_rate : array_like, default=0
         Indicative rate of guaranteed returns (e.g. US government bond rate).
@@ -82,5 +75,4 @@ def t_test(portfolio, risk_free_rate=0):
     tuple of float
     """
 
-    return scipy.stats.ttest_1samp(portfolio.returns, risk_free_rate,
-                                   alternative='greater')
+    return scipy.stats.ttest_1samp(portfolio.returns, risk_free_rate, alternative='greater')
