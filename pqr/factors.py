41cbfcb3e84cb8f1e717c4eac66c8f64232307e0
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-08-19 13:48:43 +0300

    add aligning

--- pqr/factors.py
+++ pqr/factors.py
@@ -23,6 +23,8 @@ is "lower better", whereas stocks with high ROA are usually more preferable, hen
 import numpy as np
 import pandas as pd
 
+from .utils import align
+
 __all__ = [
     'Factor',
 ]
@@ -151,6 +153,8 @@ class Factor:
         Factor
             Factor with transformed data.
         """
+
+        self.data, mask = align(self.data, mask)
 
         self.data[~mask] = np.nan
 
