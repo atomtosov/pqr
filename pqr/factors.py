1a5b483a2f2ac14c0c5e8daeb51627d0be530304
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-09-04 21:42:18 +0300

    add possibility to compare factors directly

--- pqr/factors.py
+++ pqr/factors.py
@@ -153,3 +153,21 @@ class Factor:
         self.data[~mask] = np.nan
 
         return self
+
+    def __eq__(self, other):
+        return self.data.__eq__(other)
+
+    def __ne__(self, other):
+        return self.data.__ne__(other)
+
+    def __lt__(self, other):
+        return self.data.__lt__(other)
+
+    def __gt__(self, other):
+        return self.data.__gt__(other)
+    
+    def __le__(self, other):
+        return self.data.__le__(other)
+    
+    def __ge__(self, other):
+        return self.data.__ge__(other)
