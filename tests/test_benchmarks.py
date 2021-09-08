fa1a8d7a8689feacac77f5696b7c19d2667664ab
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-09-08 13:10:31 +0300

    config fixtures

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,37 +1,27 @@
-import numpy as np
-import pandas as pd
 import pytest
 from numpy.testing import assert_allclose
+
+import numpy as np
+import pandas as pd
 
 from pqr import Benchmark
 
 
-@pytest.mark.parametrize(
-    ['index_values', 'expected'],
+@pytest.fixture
+def benchmark():
+    return Benchmark()
+
+
+@pytest.mark.parametrize('index_values, expected',
     [
-        # test 1
-        pytest.param(
-            pd.Series([1, 2, 3, 4, 5]),
-            pd.Series([np.nan, 1, 0.5, 1/3, 0.25]),
-            id='only increasing'
-        ),
-        # test 2
-        pytest.param(
-            pd.Series([5, 4, 3, 2, 1]),
-            pd.Series([np.nan, -0.2, -0.25, -1/3, -0.5]),
-            id='only decreasing'
-        ),
-        # test 3
-        pytest.param(
-            pd.Series([1, 2, 1, 3, 2]),
-            pd.Series([np.nan, 1, -0.5, 2, -1/3]),
-            id='mixed'
-        ),
+        pytest.param(pd.Series([1, 2, 3, 4, 5]), 
+                     pd.Series([np.nan, 1, 0.5, 1/3, 0.25])),
+        pytest.param(pd.Series([5, 4, 3, 2, 1]), 
+                     pd.Series([np.nan, -0.2, -0.25, -1/3, -0.5])),
+        pytest.param(pd.Series([1, 2, 1, 3, 2]),
+                     pd.Series([np.nan, 1, -0.5, 2, -1/3])),
     ]
 )
-def test_from_index(
-        index_values: pd.Series,
-        expected: pd.Series
-):
-    benchmark = Benchmark().from_index(index_values)
+def test_from_index(benchmark, index_values, expected):
+    benchmark.from_index(index_values)
     assert_allclose(benchmark.returns, expected)
--- tests/test_factors.py
+++ tests/test_factors.py
@@ -1,242 +1,112 @@
-import pandas as pd
-import numpy as np
 import pytest
 from numpy.testing import assert_allclose
+
+import pandas as pd
+import numpy as np
 
 from pqr import Factor
 
 
-@pytest.mark.parametrize(
-    ['data', 'method', 'period', 'expected'],
+@pytest.fixture
+def simple_factor():
+    return Factor(pd.DataFrame([[1, 1, 1],
+                                [2, 2, 2],
+                                [3, 3, 3],
+                                [4, 4, 4]]))
+
+
+class TestLookBack:
+    @pytest.mark.parametrize('period, expected',
+        [
+            pytest.param(1, pd.DataFrame([[1, 1, 1], 
+                                          [2, 2, 2], 
+                                          [3, 3, 3]])),
+            pytest.param(2, pd.DataFrame([[1, 1, 1], 
+                                          [2, 2, 2]])),
+        ]
+    )
+    def test_static(self, simple_factor, period, expected):
+        simple_factor.look_back(period, 'static')
+        assert_allclose(simple_factor.data, expected, equal_nan=True)
+
+    @pytest.mark.parametrize('period, expected',
+        [
+            pytest.param(1, pd.DataFrame([[1, 1, 1], 
+                                          [.5, .5, .5]])),
+            pytest.param(2, pd.DataFrame([[2, 2, 2]])),
+        ]
+    )
+    def test_dynamic(self, simple_factor, period, expected):
+        simple_factor.look_back(period, 'dynamic')
+        assert_allclose(simple_factor.data, expected, equal_nan=True)
+
+
+@pytest.mark.parametrize('period, expected',
     [
-        # test 1
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            'static', 1,
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3]]),
-            id='static period=1'
-        ),
-        # test 2
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            'dynamic', 1,
-            pd.DataFrame([[1, 1, 1],
-                          [.5, .5, .5]]),
-            id='dynamic period=1'
-        ),
-        # test 3
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            'static', 2,
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2]]),
-            id='static period=2'
-        ),
-        # test 4
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            'dynamic', 2,
-            pd.DataFrame([[2, 2, 2]]),
-            id='dynamic period=2'
-        ),
+        pytest.param(0, pd.DataFrame([[1, 1, 1], 
+                                      [2, 2, 2], 
+                                      [3, 3, 3],
+                                      [4, 4, 4]])),
+        pytest.param(1, pd.DataFrame([[1, 1, 1], 
+                                      [2, 2, 2], 
+                                      [3, 3, 3]])),
+        pytest.param(3, pd.DataFrame([[1, 1, 1]])),
     ]
 )
-def test_look_back(
-        data,
-        method,
-        period,
-        expected
-):
-    factor = Factor(data)
-    factor.look_back(period, method)
-    assert_allclose(factor.data, expected)
+def test_lag(simple_factor, period, expected):
+    simple_factor.lag(period)
+    assert_allclose(simple_factor.data, expected, equal_nan=True)
 
 
-@pytest.mark.parametrize(
-    ['data', 'period', 'expected'],
+@pytest.mark.parametrize('period, expected',
     [
-        # test 1
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            0,
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            id='period=0'
-        ),
-        # test 2
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            1,
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3]]),
-            id='period=1'
-        ),
-        # test 3
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            3,
-            pd.DataFrame([[1, 1, 1]]),
-            id='period=3'
-        ),
+        pytest.param(1, pd.DataFrame([[1, 1, 1],
+                                      [2, 2, 2],
+                                      [3, 3, 3],
+                                      [4, 4, 4]])),
+        pytest.param(2, pd.DataFrame([[1, 1, 1],
+                                      [1, 1, 1],
+                                      [3, 3, 3],
+                                      [3, 3, 3]])),
+        pytest.param(3, pd.DataFrame([[1, 1, 1],
+                                      [1, 1, 1],
+                                      [1, 1, 1],
+                                      [4, 4, 4]])),
     ]
 )
-def test_lag(
-        data: pd.DataFrame,
-        period: int,
-        expected: pd.DataFrame
-):
-    factor = Factor(data)
-    factor.lag(period)
-    assert_allclose(factor.data, expected)
+def test_hold(simple_factor, period, expected):
+    simple_factor.hold(period)
+    assert_allclose(simple_factor.data, expected, equal_nan=True)
 
 
-@pytest.mark.parametrize(
-    ['data', 'period', 'expected'],
+@pytest.mark.parametrize('mask, expected',
     [
-        # test 1
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            1,
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            id='period=1'
+        pytest.param(pd.DataFrame([[True, False, True],
+                                   [False, True, False],
+                                   [True, True, True],
+                                   [False, False, False]]),
+                     pd.DataFrame([[1, np.nan, 1],
+                                   [np.nan, 2, np.nan],
+                                   [3, 3, 3],
+                                   [np.nan, np.nan, np.nan]]),
         ),
-        # test 2
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            2,
-            pd.DataFrame([[1, 1, 1],
-                          [1, 1, 1],
-                          [3, 3, 3],
-                          [3, 3, 3]]),
-            id='period=2'
-        ),
-        # test 3
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            3,
-            pd.DataFrame([[1, 1, 1],
-                          [1, 1, 1],
-                          [1, 1, 1],
-                          [4, 4, 4]]),
-            id='period=3'
-        ),
-        # test 4
-        pytest.param(
-            pd.DataFrame([[1, 1, np.nan],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [np.nan, 4, 4]]),
-            2,
-            pd.DataFrame([[1, 1, np.nan],
-                          [1, 1, np.nan],
-                          [3, 3, 3],
-                          [3, 3, 3]]),
-            id='period=2 with nans'
-        ),
+        pytest.param(pd.DataFrame([[True, False, True],
+                                   [False, True, False],
+                                   [True, True, True],
+                                   [False, False, False],
+                                   [False, True, False]]),
+                    pd.DataFrame([[1, np.nan, 1],
+                                  [np.nan, 2, np.nan],
+                                  [3, 3, 3],
+                                  [np.nan, np.nan, np.nan]])),
+        pytest.param(pd.DataFrame([[False, True, False],
+                                   [True, True, True],
+                                   [False, False, False]], index=[1, 2, 3]),
+                     pd.DataFrame([[np.nan, 2, np.nan],
+                                   [3, 3, 3],
+                                   [np.nan, np.nan, np.nan]])),
     ]
 )
-def test_hold(
-        data: pd.DataFrame,
-        period: int,
-        expected: pd.DataFrame
-):
-    factor = Factor(data)
-    factor.hold(period)
-    assert_allclose(factor.data, expected, equal_nan=True)
-
-
-@pytest.mark.parametrize(
-    ['data', 'mask', 'expected'],
-    [
-        # test 1
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            pd.DataFrame([[True, False, True],
-                          [False, True, False],
-                          [True, True, True],
-                          [False, False, False]]),
-            pd.DataFrame([[1, np.nan, 1],
-                          [np.nan, 2, np.nan],
-                          [3, 3, 3],
-                          [np.nan, np.nan, np.nan]]),
-            id='mask\'s shape = factor\'s shapes'
-        ),
-        # test 2
-        pytest.param(
-            pd.DataFrame([[2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]], index=[1, 2, 3]),
-            pd.DataFrame([[True, False, True],
-                          [False, True, False],
-                          [True, True, True],
-                          [False, False, False]]),
-            pd.DataFrame([[np.nan, 2, np.nan],
-                          [3, 3, 3],
-                          [np.nan, np.nan, np.nan]]),
-            id='mask\'s shape > factor\'s shape'
-        ),
-        # test 3
-        pytest.param(
-            pd.DataFrame([[1, 1, 1],
-                          [2, 2, 2],
-                          [3, 3, 3],
-                          [4, 4, 4]]),
-            pd.DataFrame([[False, True, False],
-                          [True, True, True],
-                          [False, False, False]], index=[1, 2, 3]),
-            pd.DataFrame([[np.nan, 2, np.nan],
-                          [3, 3, 3],
-                          [np.nan, np.nan, np.nan]]),
-            id='mask\'s shape < factor\'s shape'
-        ),
-    ]
-)
-def test_filter(
-        data: pd.DataFrame,
-        mask: pd.DataFrame,
-        expected: pd.DataFrame
-):
-    factor = Factor(data)
-    factor.filter(mask)
-    assert_allclose(factor.data, expected, equal_nan=True)
+def test_filter(simple_factor, mask, expected):
+    simple_factor.filter(mask)
+    assert_allclose(simple_factor.data, expected, equal_nan=True)
