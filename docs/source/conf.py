3fd1197ec88dde772b494ac9dacb0557d3df5b8d
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-10-10 21:23:33 +0300

    upd to 0.2.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, eura17, atomtosov'
 author = 'eura17, atomtosov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.6'
+release = '0.2.7'
 
 
 # -- General configuration ---------------------------------------------------
