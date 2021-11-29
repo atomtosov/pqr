615b3b3e3d11f94a5648b212a6b0682e362d17b9
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-11-29 17:50:07 +0300

    draft fama macbeth

--- pqr/analytics/regressions.py
+++ pqr/analytics/regressions.py
@@ -66,4 +66,49 @@ class FamaFrenchRegression:
 
 
 class FamaMacbethRegression:
-    pass
+    def __init__(
+            self,
+            market: Benchmark,
+            benchmarks: Sequence[Benchmark],
+            rf: float = 0.0
+    ):
+        self.market = market
+        self.benchmarks = benchmarks
+        self.rf = rf
+
+    def __call__(self, portfolios: Sequence[Portfolio]) -> pd.DataFrame:
+        adjusted_portfolio_returns = [
+            adjust(portfolio.returns, self.rf)
+            for portfolio in portfolios
+        ]
+        adjusted_market_returns = adjust(self.market.returns, self.rf)
+
+        betas = []
+        for adjusted_returns in adjusted_portfolio_returns:
+            y, *x = align_many(
+                adjusted_returns,
+                adjusted_market_returns,
+                *(benchmark.returns for benchmark in self.benchmarks)
+            )
+            y = y.to_numpy()
+            x = sm.add_constant(np.array(x).T)
+
+            ols = sm.OLS(y, x)
+            est = ols.fit()
+
+            betas.append(est.params[1:])
+        print(betas)
+        betas_with_const = sm.add_constant(np.array(betas))
+        print(betas_with_const)
+        portfolio_returns = np.array(adjusted_portfolio_returns).T
+
+        lambdas = []
+        for y in portfolio_returns:
+            ols = sm.OLS(y, betas_with_const)
+            est = ols.fit()
+
+            lambdas.append(est.params[1:])
+
+        lambdas = np.array(lambdas)
+
+        return lambdas
