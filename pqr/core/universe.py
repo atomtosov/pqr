c63faf91ef52e7616a05f86160be2ad03fe535d1
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-12-08 21:12:18 +0300

    to dataclasses + remove returns calculation from there

--- pqr/core/universe.py
+++ pqr/core/universe.py
@@ -1,35 +1,16 @@
-import numpy as np
+from dataclasses import dataclass, field
+
 import pandas as pd
 
-from pqr.utils import align_many
 
-
+@dataclass
 class Universe:
-    def __init__(self, prices: pd.DataFrame):
-        self.prices = prices.astype(float)
-        self.mask = prices.notnull()
+    prices: pd.DataFrame = field(repr=False)
+    mask: pd.DataFrame = field(init=False)
+
+    def __post_init__(self):
+        self.prices = self.prices.astype(float)
+        self.mask = self.prices.notnull()
 
     def filter(self, mask: pd.DataFrame):
         self.mask &= mask
-
-    def __call__(self, positions: pd.DataFrame) -> pd.Series:
-        prices, mask, positions = align_many(self.prices, self.mask, positions)
-
-        universe_returns = prices.pct_change().to_numpy()[1:]
-
-        positions_available = positions.to_numpy()[:-1]
-        portfolio_returns = np.where(
-            mask.to_numpy()[:-1],
-            (positions_available * universe_returns), 0
-        )
-
-        dead_returns = np.where(
-            np.isnan(portfolio_returns) & ~np.isclose(positions_available, 0),
-            -positions_available, 0
-        )
-        returns = np.nansum(portfolio_returns, axis=1) + np.nansum(dead_returns, axis=1)
-
-        return pd.Series(
-            np.insert(returns, 0, 0),
-            index=positions.index.copy()
-        )
