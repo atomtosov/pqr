3fd5395941be6aa5d2b3d16f66d6d5e2926e6326
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-12-16 23:41:54 +0300

    upd tests to new metrics

--- pqr/analytics/tests.py
+++ pqr/analytics/tests.py
@@ -1,70 +1,196 @@
 from __future__ import annotations
 
-from typing import Callable, Optional, Literal, Sequence
+from dataclasses import dataclass
+from typing import Callable, Optional, Literal, Sequence, Protocol, Any
 
 import numpy as np
 import pandas as pd
 from scipy.stats import ttest_1samp
 
-import pqr
-from pqr.core import Portfolio, AllocationStep, Universe, Hold
+from pqr.core import (
+    Portfolio, Allocator, Universe,
+    Factor, Filter, LookBackPctChange, Lag, Hold, ReplaceWithNan)
 from pqr.utils import align_many
-from .utils import estimate_holding
+from .metrics import NumericMetric, TimeSeriesMetric
 
 __all__ = [
+    "Test",
     "TTest",
-    "ZeroIntelligenceTest",
-    "OpportunitiesTest"
+    "MonkeyTest",
+    "OpportunitiesTest",
 ]
 
 
-class TTest:
-    def __init__(
-            self,
-            h0: float = 0.0,
-            alternative: str = "greater"
-    ):
-        self.h0 = h0
-        self.alternative = alternative
+class Test(Protocol):
+    def test(self, portfolio: Portfolio) -> Any:
+        pass
 
-    def __call__(self, portfolio: Portfolio):
+
+@dataclass
+class TTest:
+    h0: float = 0.0
+    alternative: str = "greater"
+
+    def test(self, portfolio: Portfolio):
         return ttest_1samp(portfolio.returns, self.h0, alternative=self.alternative)
 
 
-class ZeroIntelligenceTest:
-    def __init__(
-            self,
-            universe: Universe,
-            allocation_strategy: AllocationStep | Sequence[AllocationStep],
-            target_metric: Callable[[Portfolio], pd.Series],
-            holding: Optional[int] = None,
-            n: int = 100,
-            seed: Optional[int] = None,
-    ):
-        self.universe = universe
-        self.allocation_strategy = allocation_strategy
-        self.target_metric = target_metric
-        self.n = n
-        self.holding = holding
-        self.seed = seed
+@dataclass
+class OpportunitiesTest:
+    universe: Universe
+    allocation_strategy: Allocator | Sequence[Allocator]
+    performance_metric: NumericMetric | TimeSeriesMetric
+    holding: Optional[int] = None
 
-    def __call__(self, portfolio: Portfolio) -> pd.Series:
-        random_picking = RandomPicking(
-            self.universe,
-            portfolio,
-            self.n,
-            self.holding,
-            np.random.default_rng(self.seed)
+    def test(self, portfolio: Portfolio) -> pd.Series:
+        data_to_test = self._prepare_data_to_test(portfolio)
+        best_portfolio = self._get_best_portfolio(*data_to_test)
+        worst_portfolio = self._get_worst_portfolio(*data_to_test)
+
+        if isinstance(self.performance_metric, NumericMetric):
+            performance_metric = self.performance_metric.trailing
+        else:
+            performance_metric = self.performance_metric.calculate
+
+        best_metric = performance_metric(best_portfolio)
+        worst_metric = performance_metric(worst_portfolio)
+        portfolio_metric = performance_metric(portfolio)
+        best_metric, worst_metric, portfolio_metric = align_many(
+            best_metric, worst_metric, portfolio_metric)
+
+        return (portfolio_metric - worst_metric) / (best_metric - worst_metric)
+
+    def get_best_portfolio(self, portfolio: Portfolio) -> Portfolio:
+        return self._get_best_portfolio(*self._prepare_data_to_test(portfolio))
+
+    def get_worst_portfolio(self, portfolio: Portfolio) -> Portfolio:
+        return self._get_worst_portfolio(*self._prepare_data_to_test(portfolio))
+
+    def _get_best_portfolio(
+            self,
+            long_limits: pd.DataFrame,
+            short_limits: pd.DataFrame,
+            universe: Universe,
+            holding: int
+    ) -> Portfolio:
+        best_portfolio = Portfolio(
+            *self._pick_with_forward_looking(
+                long_limits, short_limits,
+                universe, holding, "best"),
+            name="Best")
+        best_portfolio.allocate(self.allocation_strategy)
+        best_portfolio.calculate_returns(self.universe)
+        return best_portfolio
+
+    def _get_worst_portfolio(
+            self,
+            long_limits: pd.DataFrame,
+            short_limits: pd.DataFrame,
+            universe: Universe,
+            holding: int
+    ) -> Portfolio:
+        worst_portfolio = Portfolio(
+            *self._pick_with_forward_looking(
+                long_limits, short_limits,
+                universe, holding, "worst"),
+            name="Worst")
+        worst_portfolio.allocate(self.allocation_strategy)
+        worst_portfolio.calculate_returns(self.universe)
+        return worst_portfolio
+
+    def _prepare_data_to_test(self, portfolio: Portfolio):
+        longs, shorts = portfolio.get_longs(), portfolio.get_shorts()
+        longs, shorts, prices, mask = align_many(
+            longs, shorts,
+            self.universe.prices, self.universe.mask)
+
+        long_limits = longs.sum(axis=1)
+        short_limits = shorts.sum(axis=1)
+        universe = Universe(prices)
+        universe.filter(mask)
+
+        if self.holding is None:
+            holding = estimate_holding(portfolio.picks)
+        else:
+            holding = self.holding
+
+        return long_limits, short_limits, universe, holding
+
+    @staticmethod
+    def _pick_with_forward_looking(
+            long_limits: pd.DataFrame,
+            short_limits: pd.DataFrame,
+            universe: Universe,
+            holding: int,
+            how: Literal["best", "worst"]
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        returns = Factor(
+            universe.prices,
+            better="more",
+            preprocessor=[
+                Filter(universe.mask),
+                LookBackPctChange(holding),
+                Lag(-holding),
+                Hold(holding),
+                ReplaceWithNan(0),
+            ]
+        )
+        universe_returns = returns.values
+
+        longs = pd.DataFrame(
+            np.zeros_like(universe_returns, dtype=bool),
+            index=universe_returns.index.copy(),
+            columns=universe_returns.columns.copy(),
+        )
+        shorts = pd.DataFrame(
+            np.zeros_like(universe_returns, dtype=bool),
+            index=universe_returns.index.copy(),
+            columns=universe_returns.columns.copy()
         )
 
+        for date in universe_returns.index:
+            if how == "best":
+                best = universe_returns.loc[date].nlargest(long_limits.loc[date]).index
+                worst = universe_returns.loc[date].nsmallest(short_limits.loc[date]).index
+            else:
+                best = universe_returns.loc[date].nsmallest(long_limits.loc[date]).index
+                worst = universe_returns.loc[date].nlargest(short_limits.loc[date]).index
+
+            longs.loc[date, best] = True
+            shorts.loc[date, worst] = True
+
+        return longs, shorts
+
+
+@dataclass
+class MonkeyTest:
+    universe: Universe
+    allocation_strategy: Allocator | Sequence[Allocator]
+    target_metric: Callable[[Portfolio], pd.Series]
+    holding: Optional[int] = None
+    n: int = 100
+    seed: Optional[int] = None
+
+    def test(self, portfolio: Portfolio) -> pd.Series:
+        longs, shorts = portfolio.get_longs(), portfolio.get_shorts()
+        universe, longs, shorts = align_many(self.universe.mask, longs, shorts)
+
+        long_limits = np.nansum(longs.to_numpy(), axis=1)
+        short_limits = np.nansum(shorts.to_numpy(), axis=1)
+
+        rng = np.random.default_rng(self.seed)
+        if self.holding is None:
+            holding = estimate_holding(portfolio.picks)
+        else:
+            holding = self.holding
+
         random_metrics = []
-        for longs, shorts in random_picking:
+        for _ in range(self.n):
             random_portfolio = Portfolio(
-                self.universe,
-                longs=longs,
-                shorts=shorts,
-                allocation_strategy=self.allocation_strategy,
+                *self._pick_randomly(universe, long_limits, short_limits, rng, holding)
             )
+            random_portfolio.allocate(self.allocation_strategy)
+            random_portfolio.calculate_returns(self.universe)
 
             random_metrics.append(
                 self.target_metric(random_portfolio).to_numpy()
@@ -81,150 +207,50 @@ class ZeroIntelligenceTest:
             index=portfolio_metric.index
         )
 
+    @staticmethod
+    def _pick_randomly(
+            universe: pd.DataFrame,
+            long_limits: np.ndarray,
+            short_limits: np.ndarray,
+            rng: np.random.Generator,
+            holding: int,
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        universe_arr = universe.to_numpy()
+        longs = np.zeros_like(universe, dtype=bool)
+        shorts = np.zeros_like(universe, dtype=bool)
 
-class RandomPicking:
-    def __init__(
-            self,
-            universe: Universe,
-            portfolio: Portfolio,
-            n: int = 100,
-            holding: Optional[int] = None,
-            rng: Optional[np.random.Generator] = None,
-    ):
-        self.portfolio = portfolio
-        self.n = n
-        self.holding = holding if holding is not None else estimate_holding(portfolio.picks)
-        self.rng = rng if rng is not None else np.random.default_rng()
+        for i in range(0, len(universe_arr), holding):
+            available = np.where(universe_arr[i])[0]
+            n_longs, n_shorts = long_limits[i], short_limits[i]
 
-        longs, shorts = portfolio.get_longs(), portfolio.get_shorts()
-        universe, longs, shorts = align_many(universe.mask, longs, shorts)
-        self._universe = universe.to_numpy()
-        self._long_limits = np.nansum(longs.to_numpy(), axis=1)
-        self._short_limits = np.nansum(shorts.to_numpy(), axis=1)
-
-        self._i = 0
-
-    def __iter__(self) -> RandomPicking:
-        return self
-
-    def __next__(self) -> tuple[pd.DataFrame, pd.DataFrame]:
-        if self._i < self.n:
-            self._i += 1
-            return self._generate_random_picks()
-
-        raise StopIteration
-
-    def __len__(self) -> int:
-        return self.n
-
-    def _generate_random_picks(self) -> tuple[pd.DataFrame, pd.DataFrame]:
-        longs = np.zeros_like(self._universe, dtype=bool)
-        shorts = np.zeros_like(self._universe, dtype=bool)
-
-        for i in range(0, len(self._universe), self.holding):
-            available = np.where(self._universe[i])[0]
-            n_longs, n_shorts = self._long_limits[i], self._short_limits[i]
-
-            choice = self.rng.choice(
+            choice = rng.choice(
                 available,
                 size=n_longs + n_shorts,
                 replace=False
             )
 
-            hold = min(len(self._universe), i + self.holding)
+            hold = min(len(universe_arr), i + holding)
             longs[i:hold, choice[:n_longs]] = True
             shorts[i:hold, choice[n_longs:]] = True
 
         longs = pd.DataFrame(
             longs,
-            index=self.portfolio.picks.index,
-            columns=self.portfolio.picks.columns
+            index=universe.index.copy(),
+            columns=universe.columns.copy()
         )
         shorts = pd.DataFrame(
             shorts,
-            index=self.portfolio.picks.index,
-            columns=self.portfolio.picks.columns
+            index=universe.index.copy(),
+            columns=universe.columns.copy()
         )
 
         return longs, shorts
 
 
-class OpportunitiesTest:
-    def __init__(
-            self,
-            universe: Universe,
-            allocation_strategy: AllocationStep | Sequence[AllocationStep],
-            target_metric: Callable[[Portfolio], pd.Series],
-            holding: Optional[int] = None
-    ):
-        self.universe = universe
-        self.allocation_strategy = allocation_strategy
-        self.target_metric = target_metric
-        self.holding = holding
+def estimate_holding(picks: pd.DataFrame) -> int:
+    diff = np.diff(picks.to_numpy(), axis=0)
+    rebalancings_long = (diff == 1).any(axis=1).sum()
+    rebalancings_short = (diff == -1).any(axis=1).sum()
+    avg_rebalacings = (rebalancings_long + rebalancings_short) / 2
 
-    def __call__(self, portfolio: Portfolio) -> pd.Series:
-        holding = estimate_holding(portfolio.picks) if self.holding is None else self.holding
-
-        best_portfolio = pqr.Portfolio(
-            self.universe,
-            *ProphetPickingStrategy(self.universe, holding, "best")(portfolio),
-            allocation_strategy=self.allocation_strategy
-        )
-        worst_portfolio = pqr.Portfolio(
-            self.universe,
-            *ProphetPickingStrategy(self.universe, holding, "worst")(portfolio),
-            allocation_strategy=self.allocation_strategy
-        )
-
-        best_metric = self.target_metric(best_portfolio)
-        worst_metric = self.target_metric(worst_portfolio)
-        portfolio_metric = self.target_metric(portfolio)
-
-        return (portfolio_metric - worst_metric) / (best_metric - worst_metric)
-
-
-class ProphetPickingStrategy:
-    def __init__(
-            self,
-            universe: Universe,
-            holding: int,
-            way: Literal["best", "worst"]
-    ):
-        self.universe = universe
-        self.holding = holding
-        self.way = way
-
-    def __call__(self, portfolio: Portfolio) -> tuple[pd.DataFrame, pd.DataFrame]:
-        longs, shorts = portfolio.get_longs(), portfolio.get_shorts()
-        longs, shorts, universe, prices = align_many(longs, shorts, self.universe.mask, self.universe.prices)
-
-        long_limits = longs.sum(axis=1)
-        short_limits = shorts.sum(axis=1)
-
-        universe_returns = prices.pct_change(self.holding).shift(-self.holding)
-        universe_returns = Hold(self.holding)(universe_returns)
-        universe_returns[~universe] = np.nan
-
-        longs = pd.DataFrame(
-            np.zeros_like(longs, dtype=bool),
-            index=longs.index,
-            columns=longs.columns
-        )
-        shorts = pd.DataFrame(
-            np.zeros_like(shorts, dtype=bool),
-            index=shorts.index,
-            columns=shorts.columns
-        )
-
-        for date in universe.index:
-            if self.way == "best":
-                best = universe_returns.loc[date].nlargest(long_limits.loc[date]).index
-                worst = universe_returns.loc[date].nsmallest(short_limits.loc[date]).index
-            else:
-                best = universe_returns.loc[date].nsmallest(long_limits.loc[date]).index
-                worst = universe_returns.loc[date].nlargest(short_limits.loc[date]).index
-
-            longs.loc[date, best] = True
-            shorts.loc[date, worst] = True
-
-        return longs, shorts
+    return round(len(diff) / avg_rebalacings)
