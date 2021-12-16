9cb35423bfbf9a518b402df7c0dbc06db70a5e42
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2021-12-16 23:36:32 +0300

    to dataclasses + rename graph to chart

--- pqr/analytics/dashboards.py
+++ pqr/analytics/dashboards.py
@@ -1,115 +1,92 @@
 from __future__ import annotations
 
-from typing import Sequence, Protocol, Optional
+from dataclasses import dataclass
+from typing import Sequence, Optional, Protocol
 
 import matplotlib.pyplot as plt
 import pandas as pd
 from IPython.display import display
 
 from pqr.core import Portfolio, Benchmark
+from .metrics import NumericMetric, TimeSeriesMetric
 
 __all__ = [
     "Dashboard",
-    "Graph",
+    "Chart",
     "Table",
 ]
 
 
-class NumericMetric(Protocol):
-    def fancy(self, portfolio: Portfolio) -> str:
-        pass
-
-    @property
-    def fancy_name(self, portfolio: Portfolio) -> str:
+class Displayable(Protocol):
+    def display(self, portfolios: Sequence[Portfolio]) -> None:
         pass
 
 
-class TimeSeriesMetric(Protocol):
-    def __call__(self, portfolio: Portfolio) -> pd.Series:
-        pass
-
-    def fancy(self, portfolio: Portfolio) -> pd.Series:
-        pass
-
-    @property
-    def fancy_name(self, portfolio: Portfolio) -> str:
-        pass
-
-
+@dataclass
 class Dashboard:
-    def __init__(self, *items: Graph | Table):
-        self.items = items
+    items: Sequence[Displayable]
 
-    def __call__(self, portfolios: Sequence[Portfolio]) -> None:
+    def display(self, portfolios: Sequence[Portfolio]) -> None:
         for item in self.items:
-            item(portfolios)
+            item.display(portfolios)
 
 
-class Graph:
-    def __init__(
-            self,
-            metric: TimeSeriesMetric,
-            benchmark: Optional[Benchmark] = None,
-            log_scale: bool = False,
-            figsize: tuple[int, int] = (10, 10)
-    ):
-        self.metric = metric
-        self.benchmark = benchmark
-        self.log_scale = log_scale
-        self.figsize = figsize
+@dataclass
+class Chart:
+    metric: NumericMetric | TimeSeriesMetric
+    benchmark: Optional[Benchmark] = None
+    log_scale: bool = False
+    figsize: tuple[int, int] = (10, 10)
 
-    def __call__(self, portfolios: Sequence[Portfolio]) -> None:
+    def display(self, portfolios: Sequence[Portfolio]) -> None:
         plt.figure(figsize=self.figsize)
 
+        if isinstance(self.metric, NumericMetric):
+            metric = self.metric.trailing
+            metric_name = f"Trailing {self.metric.name}"
+        else:
+            metric = self.metric.calculate
+            metric_name = self.metric.name
+
         for portfolio in portfolios:
-            plt.plot(
-                self.metric(portfolio) if self.log_scale else
-                self.metric.fancy(portfolio),
-                label=portfolio.name
-            )
+            plt.plot(metric(portfolio), label=portfolio.name)
 
         if self.benchmark is not None:
             starts_from = min(portfolio.returns.index[0] for portfolio in portfolios)
             benchmark_returns = self.benchmark.returns[starts_from:]
             benchmark_returns.iloc[0] = 0.0
-            benchmark = Benchmark(
-                benchmark_returns,
-                name=self.benchmark.name
-            )
+            benchmark = Benchmark(benchmark_returns, name=self.benchmark.name)
+
             plt.plot(
-                self.metric(benchmark) if self.log_scale else
-                self.metric.fancy(benchmark),
+                metric(benchmark),
                 label=self.benchmark.name,
                 color="gray",
-                alpha=0.8,
-            )
+                alpha=0.8)
 
         if self.log_scale:
             plt.yscale("symlog")
 
-        plt.title(f"Portfolios {self.metric.fancy_name}")
+        plt.title(f"Portfolios {metric_name}")
         plt.xlabel("Date")
-        plt.ylabel(f"{self.metric.fancy_name}{' (log scale)' if self.log_scale else ''}")
+        plt.ylabel(f"{metric_name}{' (log scale)' if self.log_scale else ''}")
         plt.legend()
         plt.grid()
 
         plt.show()
 
 
+@dataclass
 class Table:
-    def __init__(self, *metrics: NumericMetric):
-        self.metrics = metrics
+    metrics: Sequence[NumericMetric]
 
-    def __call__(self, portfolios: Sequence[Portfolio]) -> None:
-        metrics_table = {}
+    def display(self, portfolios: Sequence[Portfolio]) -> None:
+        metrics = {}
         for metric in self.metrics:
-            metrics_table[metric.fancy_name] = [
-                metric.fancy(portfolio) for portfolio in portfolios
-            ]
+            metrics[metric.name] = [
+                metric.fancy(portfolio) for portfolio in portfolios]
 
-        display(
-            pd.DataFrame(
-                metrics_table,
-                index=[portfolio.name for portfolio in portfolios]
-            ).T
-        )
+        metrics_table = pd.DataFrame(
+            metrics,
+            index=[portfolio.name for portfolio in portfolios]).T
+
+        display(metrics_table)
