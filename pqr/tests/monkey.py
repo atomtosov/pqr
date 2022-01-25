2f83d5e5728846065db3f48124e514042de280ba
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2022-01-25 22:27:44 +0300

    remove holding from params

--- pqr/tests/monkey.py
+++ pqr/tests/monkey.py
@@ -12,7 +12,6 @@ import pandas as pd
 
 from pqr.core import backtest_portfolio
 from pqr.utils import (
-    estimate_holding,
     align,
     partial,
     longs_from_portfolio,
@@ -21,37 +20,29 @@ from pqr.utils import (
 
 
 def monkey_test(
-        portfolio: pd.DataFrame,
+        base_portfolio: pd.DataFrame,
         prices: pd.DataFrame,
         universe: pd.DataFrame,
         allocation: Optional[Callable[[pd.DataFrame], pd.DataFrame]] = None,
         n: int = 100,
-        holding: Optional[int] = None,
-        random_seed: Optional[int] = None
+        random_seed: Optional[int] = None,
 ) -> pd.Series:
-    if holding is None:
-        holding = estimate_holding(portfolio)
-
-    rng = np.random.default_rng(random_seed)
-
     random_portfolio_factory = get_random_portfolio_factory(
-        base_portfolio=portfolio,
+        base_portfolio=base_portfolio,
         prices=prices,
         universe=universe,
         allocation=allocation,
-        holding=holding,
-        rng=rng,
+        rng=np.random.default_rng(random_seed),
     )
-
     random_returns = np.array([
         random_portfolio_factory()["returns"] for _ in range(n)
     ]).T
 
-    outperform = random_returns <= portfolio["returns"].to_numpy()[:, np.newaxis]
+    outperform = random_returns <= base_portfolio["returns"].to_numpy()[:, np.newaxis]
 
     monkey_est = pd.Series(
         np.nanmean(outperform, axis=1)[1:],
-        index=portfolio.returns.index.copy()[1:]
+        index=base_portfolio.returns.index.copy()[1:]
     )
     monkey_est.index.name = "Monkey"
 
@@ -63,22 +54,20 @@ def get_random_portfolio_factory(
         prices: pd.DataFrame,
         universe: pd.DataFrame,
         allocation: Callable[[pd.DataFrame], pd.DataFrame],
-        holding: int,
         rng: np.random.Generator,
 ) -> Callable[[], pd.DataFrame]:
-    longs, shorts = longs_from_portfolio(base_portfolio), shorts_from_portfolio(base_portfolio)
+    longs = longs_from_portfolio(base_portfolio)
+    shorts = shorts_from_portfolio(base_portfolio)
     long_random_strategy = partial(
         pick_randomly,
         universe=universe,
         holdings_number=longs.sum(axis=1),
-        holding=holding,
         rng=rng,
     )
     short_random_strategy = partial(
         pick_randomly,
         universe=universe,
         holdings_number=shorts.sum(axis=1),
-        holding=holding,
         rng=rng,
     )
 
@@ -94,23 +83,21 @@ def get_random_portfolio_factory(
 def pick_randomly(
         universe: pd.DataFrame,
         holdings_number: pd.Series,
-        holding: int,
-        rng: np.random.Generator
+        rng: np.random.Generator,
 ) -> pd.DataFrame:
     universe, holdings_number = align(universe, holdings_number)
     universe_array = universe.to_numpy()
     holdings_number_array = holdings_number.to_numpy()
 
     choices = np.zeros_like(universe_array, dtype=bool)
-    stop = len(choices)
-    for i in range(0, stop, holding):
+    for i in range(0, len(choices)):
         tradable = np.where(universe_array[i])[0]
         choice = rng.choice(
             tradable,
             size=holdings_number_array[i],
-            replace=False
+            replace=False,
         )
-        choices[i:min(stop, i + holding), choice] = True
+        choices[i, choice] = True
 
     return pd.DataFrame(
         choices,
